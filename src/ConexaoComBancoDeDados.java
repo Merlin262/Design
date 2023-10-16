fd11fa2a3b2ee5e64b2040a40b73f386ecd841ed
João Merlin <88944432+Merlin262@users.noreply.github.com>
2023-10-16 19:27:44 -0300

    Update ConexaoComBancoDeDados.java

--- src/ConexaoComBancoDeDados.java
+++ src/ConexaoComBancoDeDados.java
@@ -3,8 +3,8 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 public class ConexaoComBancoDeDados {
     private static String url = "jdbc:mysql://localhost:3306/Clinica";
-    private static String usuario = "root";
-    private static String senha = "jgsm2210";
+    private static String usuario = "";
+    private static String senha = "";
 
     public static Connection obterConexao() throws SQLException {
         return DriverManager.getConnection(url, usuario, senha);
